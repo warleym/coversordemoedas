6dfbc0c815f3e9d2662b508ed2748f6e357b5403
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-03-28 00:15:04 -0300

    Criação dos elementos da aplicação

--- app/src/main/java/br/com/progiv/conversormoedas/MainActivity.java
+++ app/src/main/java/br/com/progiv/conversormoedas/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.progiv.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.editValor);
+        valor.getText();
+
+
+
     }
 }
