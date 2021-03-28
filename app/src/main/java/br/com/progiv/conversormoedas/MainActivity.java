34a79896981fecac671c9839397a16e3609aadfa
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-03-28 01:33:15 -0300

    Código do botão Calculcar

--- app/src/main/java/br/com/progiv/conversormoedas/MainActivity.java
+++ app/src/main/java/br/com/progiv/conversormoedas/MainActivity.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,31 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.ButtonCalculate = findViewById(R.id.button_calculate);
 
+        this.mViewHolder.ButtonCalculate.setOnClickListener(this);
 
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.editValue.getText().toString();
+            if ("".equals(value)){
+                //Mostra Mensagem pro usuário
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+
+            }else {
+                Double real = Double.valueOf(value);
+                this.mViewHolder.textDolar.setText(String.format("%.2f",(real / 4)));
+                this.mViewHolder.textEuro.setText(String.format("%.2f",(real / 5)));
+            }
+        }
+
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
     }
 
     private static class ViewHolder {
